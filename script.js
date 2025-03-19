d360731670bf80bc19bc246026b362ea24098002
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-03-19 11:41:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your code here
+
+const images = document.getElementsByClassName("image");
+let draggedElement = null;
+
+for(let i=0; i<images.length; i++){
+	images[i].addEventListener("dragstart", (e) => {
+		draggedElement = e.target;
+	});
+
+	images[i].addEventListener("dragover", (e) => {
+		e.preventDefault();
+	});
+
+	images[i].addEventListener("drop", (e) => {
+		// e.preventDefault();
+		let draggedBg = window.getComputedStyle(draggedElement).backgroundImage;
+		let targetBg = window.getComputedStyle(e.target).backgroundImage;
+		console.log(draggedBg);
+		console.log(targetBg);
+		if (draggedBg !== targetBg) {
+			draggedElement.style.backgroundImage = targetBg;
+			e.target.style.backgroundImage = draggedBg;
+			
+			let tempInnerText = draggedElement.innerText;
+			draggedElement.innerText = e.target.innerText;
+			e.target.innerText = tempInnerText;
+	    }
+	})
+}
